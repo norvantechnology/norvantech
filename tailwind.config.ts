cc2c362b5e8810fa20294427426376a2e176dde8
Dhaval <Dhaval>
2025-03-04 22:19:29 +0530

    tailwind file change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
-
+/** @type {import('tailwindcss').Config} */
 const config: Config = {
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
